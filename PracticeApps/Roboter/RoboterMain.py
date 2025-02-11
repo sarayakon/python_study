c7017b1871675df9a8286185d3a24f8ba0d68df8
asa085 <rule085remein@gmail.com>
2025-02-12 00:01:20 +0900

    fix robomain

--- PracticeApps/Roboter/RoboterMain.py
+++ PracticeApps/Roboter/RoboterMain.py
@@ -5,12 +5,6 @@ class RoboterMain:
     def __init__(self):
         self.roboter_check_name = RoboterCheckName()
 
-        # set yes answers
-        self.yes_words = ['yes', 'Yes', 'YES']
-
-        # set no answers
-        self.no_words = ['no', 'No', 'NO']
-
     def run_app(self):
         self.roboter_check_name.check_customer_name()
 
