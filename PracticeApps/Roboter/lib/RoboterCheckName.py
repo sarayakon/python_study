3d69400a70d6c77ea74bafa165d2005afda8047d
asa085 <rule085remein@gmail.com>
2025-02-15 01:11:12 +0900

    fix CheckName module

--- PracticeApps/Roboter/lib/RoboterCheckName.py
+++ PracticeApps/Roboter/lib/RoboterCheckName.py
@@ -1,5 +1,4 @@
 # Check Customer Name for Roboter App
-from PracticeApps.Roboter.config import YES_WORDS, NO_WORDS
 
 
 class RoboterCheckName:
@@ -7,13 +6,6 @@ class RoboterCheckName:
     def __init__(self):
         self.customer_name = ""
         self.answer = ""
-        self.second_answer = ""
-
-        # set yes answers
-        self.yes_words = YES_WORDS
-
-        # set no answers
-        self.no_words = NO_WORDS
 
     def check_customer_name(self):
         answer_number = 0
@@ -21,13 +13,11 @@ class RoboterCheckName:
             if answer_number == 0:
                 print(f"Hello! I'm Roboko. What's your name?")
                 self.customer_name = input().strip()
-            print(f'Thank you for your input!Is your name {self.customer_name}? Please answer {self.yes_words[0]} or '
-                  f'{self.no_words[0]}.')
-            self.second_answer = input().strip()
-            if self.second_answer in self.yes_words:
-                print(f'registered your name as {self.customer_name}.')
+            print(f'Thank you for your input!Is your name {self.customer_name}? Please answer yes or no')
+            self.answer = input().strip().lower()
+            if self.answer == 'yes':
                 break
-            elif self.second_answer in self.no_words:
+            elif self.answer == 'no':
                 print(f'Please enter your name again.')
                 answer_number = 0
             else:
@@ -35,3 +25,5 @@ class RoboterCheckName:
                 answer_number += 1
         if answer_number >= 5:
             print(f'You have exceeded the limit of incorrect answers. Please restart the app.')
+        else:
+            return self.customer_name
