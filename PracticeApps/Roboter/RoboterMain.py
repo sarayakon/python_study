304880d719563e3766e7e322671d7d7f5db8ff5b
asa085 <rule085remein@gmail.com>
2025-02-15 01:10:31 +0900

    create registerName file, fix main, delete config

--- PracticeApps/Roboter/RoboterMain.py
+++ PracticeApps/Roboter/RoboterMain.py
@@ -1,13 +1,19 @@
 from PracticeApps.Roboter.lib.RoboterCheckName import RoboterCheckName
+from PracticeApps.Roboter.lib.RegisterName import RegisterName
 
 
 class RoboterMain:
     def __init__(self):
+        self.csv_path = './csv'
+
         self.roboter_check_name = RoboterCheckName()
+        self.register_mame = RegisterName(self.csv_path)
 
     def run_app(self):
-        self.roboter_check_name.check_customer_name()
+        customer_name = self.roboter_check_name.check_customer_name()
+        self.register_mame.register_name(customer_name)
 
 
-roboter = RoboterMain()
-roboter.run_app()
+if __name__ == "__main__":
+    roboter = RoboterMain()
+    roboter.run_app()
--- PracticeApps/Roboter/config.py
+++
@@ -1,5 +0,0 @@
-# config
-
-# Handling checking customer name
-YES_WORDS = ['yes', 'Yes', 'YES']
-NO_WORDS = ['no', 'No', 'NO']
